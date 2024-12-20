c28785f117ccbd4a7c924c8fec5625237dd253df
Jarno <Jarno.degano@students.fhnw.ch>
2024-12-20 20:48:24 +0100

    adjusted css.

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -3,8 +3,8 @@ import {
   Frontend_skill,
   Full_stack,
   Other_skill,
+  Skill_data,
 } from "@/constants/Index";
-import { transform } from "next/dist/build/swc";
 import React from "react";
 import SkillDataProvider from "../sub/SkillDataProvider";
 import SkillText from "../sub/SkillText";
@@ -13,66 +13,79 @@ const Skills = () => {
   return (
     <section
       id="skills"
-      className="flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden py-20"
-      style={{ transform: "scale(0.9)" }}
+      className="flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden pb-[27rem] py-20"
     >
       <SkillText />
+
       <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-        {Frontend_skill.map((image, index) => (
-          <SkillDataProvider
-            key={index}
-            src={image.Image}
-            width={image.width}
-            height={image.height}
-            index={index}
-          />
-        ))}
+      {Skill_data.map((image, index) => (
+        <SkillDataProvider
+        key={index}
+        src={image.Image}
+        width={image.width}
+        height={image.height}
+        index={index}
+        />
+      ))}
+      </div>
+
+      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
+      {Frontend_skill.map((image, index) => (
+        <SkillDataProvider
+        key={index}
+        src={image.Image}
+        width={image.width}
+        height={image.height}
+        index={index}
+        />
+      ))}
       </div>
       <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-        {Backend_skill.map((image, index) => (
-          <SkillDataProvider
-            key={index}
-            src={image.Image}
-            width={image.width}
-            height={image.height}
-            index={index}
-          />
-        ))}
+      {Backend_skill.map((image, index) => (
+        <SkillDataProvider
+        key={index}
+        src={image.Image}
+        width={image.width}
+        height={image.height}
+        index={index}
+        />
+      ))}
       </div>
       <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-        {Full_stack.map((image, index) => (
-          <SkillDataProvider
-            key={index}
-            src={image.Image}
-            width={image.width}
-            height={image.height}
-            index={index}
-          />
-        ))}
+      {Full_stack.map((image, index) => (
+        <SkillDataProvider
+        key={index}
+        src={image.Image}
+        width={image.width}
+        height={image.height}
+        index={index}
+        />
+      ))}
       </div>
       <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-        {Other_skill.map((image, index) => (
-          <SkillDataProvider
-            key={index}
-            src={image.Image}
-            width={image.width}
-            height={image.height}
-            index={index}
-          />
-        ))}
+      {Other_skill.map((image, index) => (
+        <SkillDataProvider
+        key={index}
+        src={image.Image}
+        width={image.width}
+        height={image.height}
+        index={index}
+        />
+      ))}
       </div>
+
       <div className="w-full h-full absolute">
-        <div className="w-full h-full z-[-10] opacity-30 absolute flex items-center justify-center bg-cover">
-          <video
-            className="w-full h-auto"
-            preload="false"
-            playsInline
-            loop
-            muted
-            autoPlay
-            src="/cards-video.webm"
-          />
-        </div>
+      <div className="w-full h-full z-[-10] opacity-30 absolute flex items-center justify-center bg-cover">
+        <video
+        className="w-[60%] h-auto opacity-70 pt-[10rem]"
+        preload="false"
+        playsInline
+        loop
+        muted
+        autoPlay
+        src="/cards-video.webm"
+        />
+      </div>
       </div>
     </section>
   );
